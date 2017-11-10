62fcad8a3195c5d5ae09763f245f9d4cbc528fd8
Jared East <JaredEastPhoto@gmail.com>
2017-11-10 16:51:03 -0500

    Click Counter working

--- js/main.js
+++ js/main.js
@@ -1,14 +1,7 @@
-var pageHeading = document.getElementById('pageHeading');
-
-pageHeading.addEventListener('click', function () {
-    var myElement = document.createElement('article');
-    myElement.innerHTML = '<p>This is click number 1</p>';
-    document.getElementsByClassName('container')[0].appendChild(myElement);
-});
-
-
-pageHeading.addEventListener('click', function () {
-    var myElement = document.createElement('article');
-    myElement.innerHTML = '<p>This is click number 2</p>';
-    document.getElementsByClassName('container')[0].appendChild(myElement);
-});
+var button = document.getElementById('clickme'),
+    count = 0;
+button.onclick = function () {
+    count += 1;
+    button.innerHTML = 'Click Me: ' +
+    count;
+};
